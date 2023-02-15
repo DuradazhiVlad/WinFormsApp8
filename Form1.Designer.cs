93a84ea5a23455173d8fcf5b60a1704d01dbd942
DuradazhiVlad <dvlad1995p@gmail.com>
2023-02-15 16:26:46 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WinFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cтатик";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateTimePicker1.Location = new System.Drawing.Point(33, 183);
+            this.dateTimePicker1.MinDate = new System.DateTime(1995, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTimePicker1.Size = new System.Drawing.Size(268, 55);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 69);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вирахувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(33, 311);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 55);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(259, 311);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(162, 55);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(456, 311);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(210, 55);
+            this.numericUpDown3.TabIndex = 1;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(752, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(287, 69);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Вирахувати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 622);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace WinFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }    
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random random = new Random();
+
+            int x = button1.Location.X;               
+
+            int newX = random.Next(this.Width - button1.Width);
+            
+            int stepX = (newX - x) ;
+            
+            button1.Location = new Point(newX);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime date = new DateTime();
+            date = dateTimePicker1.Value;
+            if (date.DayOfWeek==DayOfWeek.Wednesday)
+            {
+                MessageBox.Show("Сьогодні середа");
+            }
+            if (date.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                MessageBox.Show("Сьогодні вівторок");
+            }
+            if (date.DayOfWeek == DayOfWeek.Thursday)
+            {
+                MessageBox.Show("Сьогодні четвер");
+            }
+            if (date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                MessageBox.Show("Сьогодні неділя");
+            }
+            if (date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                MessageBox.Show("Сьогодні субота");
+            }
+            if (date.DayOfWeek == DayOfWeek.Monday)
+            {
+                MessageBox.Show("Сьогодні понеділок");
+            }
+            if (date.DayOfWeek == DayOfWeek.Friday)
+            {
+                MessageBox.Show("Сьогодні п'ятниця");
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decimal date1 = numericUpDown1.Value;
+            decimal date2 = numericUpDown2.Value;
+            decimal date3 = numericUpDown3.Value;
+
+
+            DateTime date = new DateTime(((int)date3),((int)date2),((int)date1));
+
+            MessageBox.Show(date.ToString());
+            if (date.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                MessageBox.Show("Сьогодні середа");
+            }
+            if (date.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                MessageBox.Show("Сьогодні вівторок");
+            }
+            if (date.DayOfWeek == DayOfWeek.Thursday)
+            {
+                MessageBox.Show("Сьогодні четвер");
+            }
+            if (date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                MessageBox.Show("Сьогодні неділя");
+            }
+            if (date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                MessageBox.Show("Сьогодні субота");
+            }
+            if (date.DayOfWeek == DayOfWeek.Monday)
+            {
+                MessageBox.Show("Сьогодні понеділок");
+            }
+            if (date.DayOfWeek == DayOfWeek.Friday)
+            {
+                MessageBox.Show("Сьогодні п'ятниця");
+            }
+        }
+    }
+}
